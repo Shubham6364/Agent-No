05557de25529d8f3819e80ba8022acfbad6a6cbb
adi <adi836386@gmail.com>
2022-06-10 10:47:11 +0530

    static

--- agent/settings.py
+++ agent/settings.py
@@ -150,7 +150,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATIC_ROOT = 'C:\project\agent\app\static'
+
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 
