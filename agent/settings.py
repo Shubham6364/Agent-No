35ef16391a62edd9acf940fa219874c97e6916a5
adi <adi836386@gmail.com>
2022-06-10 08:26:42 +0530

    staticfile

--- agent/settings.py
+++ agent/settings.py
@@ -146,7 +146,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
+
 STATIC_ROOT = 'C:\project\agent\app\static'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
