84fa29d7f90fb10f247d1241db729c6a56142f07
adi <adi836386@gmail.com>
2022-06-10 10:49:46 +0530

    STATIC

--- agent/settings.py
+++ agent/settings.py
@@ -151,7 +151,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATICFILES_DIR = [
+    os.path.join(BASE_DIR, "static")
+]
 
 
 
