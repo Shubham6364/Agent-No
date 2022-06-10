4f1a56549d5e1e7d2d080a28d67dc69cad6d73bf
adi <adi836386@gmail.com>
2022-06-10 11:13:42 +0530

    first commit

--- agent/settings.py
+++ agent/settings.py
@@ -10,8 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import imp
 from pathlib import Path
 import os
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -181,3 +183,4 @@ SITE_ID = 1
 
 LOGIN_REDIRECT_URL = "home"
 
+django_heroku.settings(locals())
